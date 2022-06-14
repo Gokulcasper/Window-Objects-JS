601a768240815dac3096fd8761d2218780e8e45b
Gokulcasper <ronitcasper215@gmail.com>
2022-06-14 13:36:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// window.open("https://google.com");
+
+// window.moveTo(500,100);
+
+// window.resizeTo(1000,500);
+
+// window.close();
+
+// window.alert("Hello World");
+
+// window.prompt("what is your name");
+
+// window.print("HTML DOM");
+
+console.log(console);
+
+// console.error(console);
+
+// console.warn(console);
+
+// console.log(window);
